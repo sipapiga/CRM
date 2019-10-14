0494f13c44fa6e8eb18e791f1a9994e381f182cf
Pat <sipapat.prasopsap@gmail.com>
2019-10-14 23:36:48 +0200

    Added remove method and save data in LocalStorage method

--- js/customer.js
+++ js/customer.js
@@ -1,34 +1,60 @@
 class CustomerList {
     constructor() {
         this.list = [];
-        this.customer_id = 0;
+        this.customer_id = 1;
     }
-    addCustomer(customerinfo) {
-        this.customer_id++;
-        let customer_list = new Customer(customerinfo.name.first, customerinfo.name.last, customerinfo.dob.date, customerinfo.phone, customerinfo.picture.thumbnail, customerinfo.email, this.customer_id, customerinfo.location, customerinfo.company.name);
-        this.list.push(customer_list);
-    }
+
     displayCustomer() {
-        this.list.forEach(list => {
-            this.addCustomerToList(list);
+        const dummy = this.getContactsFromLocalStorage();
+        dummy.forEach(customer => {
+            this.addCustomerToList(customer);
+            console.log(customer);
         });
     }
+    setDummyCustomer() {
+        //dummy customer
+        for (let customerinfo of contacts) {
+            let customer = new Customer(customerinfo.name.first, customerinfo.name.last, customerinfo.dob.date, customerinfo.phone, customerinfo.picture.thumbnail, customerinfo.email, this.customer_id, customerinfo.location, customerinfo.company.name);
+            this.list.push(customer);
+            this.customer_id++;
+        }
+        localStorage.setItem('Customers', JSON.stringify(this.list));
+    }
+
+    getContactsFromLocalStorage() {
+        let contactsJson = JSON.parse(localStorage.getItem('Customers'));
+        console.log(contactsJson);
+        return contactsJson;
+    }
+
+    RemoveContactFromLocalStorage(num) {
+        const customerList = this. getContactsFromLocalStorage();
+        customerList.forEach((contact, index) => {
+            if (contact.id == num) {
+                customerList.splice(index, 1);  //remove from localStorage
+                this.list.splice(index, 1);  //remove from CustomerList class
+            }
+        });
+        localStorage.setItem('Customers', JSON.stringify(customerList));
+
+        console.log(this.list);
+    }
+    //Append table row 
     addCustomerToList(list) {
         const cusDiv = document.querySelector("#myTable");
         const row = document.createElement("tr");
-        row.innerHTML = `<td><img src="${list.photo}" class="img-circle thumb-sm" id="photo"/></td>
+        row.innerHTML = `<td><p>${list.id}</p></td>
+                <td><img src="${list.photo}" class="img-circle thumb-sm" id="photo"/></td>
                 <td><p id="firstName">${ list.name}</p></td>
                 <td><p id="lastName">${ list.lastname} </p></td>
                 <td><a href="#" id="email">${ list.email} </a></td>
                 <td><p id="tel">${ list.tel} </p> </td>
-                <td><a href="#" class="btn btn-success btn-sm info">Show info</a></td>
+                <td><a href="#" class="btn btn-success btn-sm info">Info</a></td>
                 <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
                 `;
         cusDiv.appendChild(row);
     }
-
-    addContact() {
-        this.customer_id++;
+    addNewContact() {
         const name = document.querySelector('#inputName').value;
         const lastname = document.querySelector('#inputLastname').value;
         const email = document.querySelector('#inputEmail').value;
@@ -38,21 +64,24 @@ class CustomerList {
 
         let newCus = new Customer(name, lastname, "", tel, 'https://randomuser.me/api/portraits/thumb/women/5.jpg', email, this.customer_id, address, company);
         this.list.push(newCus);
-        this.addCustomerToList(newCus);
+        this.addCustomerToList(newCus, this.customer_id);
+        localStorage.setItem('Customers', JSON.stringify(this.list));
         this.clearFieldInput();
         this.hideAddcontactModal();
+        this.customer_id++;
         console.log(this.list);
     }
+    //delete from UI
     deleteContactList(e) {
         if (e.classList.contains('delete')) {
             e.parentElement.parentElement.remove();
         }
     }
+    //Show customer info here
     showInfo(e) {
-        if (e.classList.contains('delete')) {
-            e.parentElement.parentElement.remove();
-        }
+        console.log("SHow info here");
     }
+
     clearFieldInput() {
         document.querySelector('#inputName').value = "";
         document.querySelector('#inputLastname').value = "";
@@ -61,6 +90,7 @@ class CustomerList {
         document.querySelector('#inputCompany').value = "";
         document.querySelector('#inputAddress').value = "";
     }
+    //not really work
     hideAddcontactModal() {
         let modal = document.getElementById('addContact');
         modal.classList.remove('show');
--- js/main.js
+++ js/main.js
@@ -2,21 +2,27 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
     let customerList = new CustomerList();
-
-    for (let customerinfo of contacts) {
-        customerList.addCustomer(customerinfo);
-    }
-    customerList.displayCustomer();
     console.log(customerList);
-    console.log(contacts);
 
-    document.getElementById('saveBtn').addEventListener('click', e => customerList.addContact());
-    document.getElementById('cancel').addEventListener('click', e => {
-       customerList.hideAddcontactModal();
+    customerList.displayCustomer();
+
+    document.getElementById('saveBtn').addEventListener('click', e => {
+        customerList.addNewContact();
     });
-
+    document.getElementById('cancel').addEventListener('click', function () {
+        customerList.hideAddcontactModal();
+    });
+    //Delete customer from UI and localStorage
     document.getElementById('myTable').addEventListener('click', e => {
         console.log(e.target);
-       customerList.deleteContactList(e.target);
-     });
+        customerList.deleteContactList(e.target);
+        let id = e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
+        console.log("id " + id);
+        customerList.RemoveContactFromLocalStorage(id);
+    });
+     //Show customer info
+    document.getElementById('myTable').addEventListener('click', e => {
+        console.log(e.target);
+        customerList.showInfo(e.target);
+    });
 });
