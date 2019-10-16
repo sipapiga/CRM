d6d3be2944c08e6a4d5099376e39ff17b8b0eb1b
BarbaraMaes <maesbarbara92@gmail.com>
2019-10-16 21:10:44 +0200

    Added functionality to calender and both lists

--- calender.js
+++ calender.js
@@ -1,31 +1,142 @@
+class calenderItem { 
+    constructor(text, dateTime, element){
+        this.text = text;
+        this.date = dateTime;
+        this.element = element;
+    }
+}
+class dateTime{
+    constructor(day, month, year, hours, minutes){
+        this.day = day; 
+        this.month = month; 
+        this.year = year; 
+        this.hours = hours, 
+        this.minutes = minutes; 
+    }
+}
+let eventList = [];
+    
 let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 let days = [31,28,31,30,31,31,30,31,30,31,30,31];
-
+    
 let month = new Date().getMonth();
 let year = new Date().getFullYear();
+    
 
-displayMonth();
+$(document).ready(function(){
+    $(".icon").click(function(){
+        if($(this).hasClass("next")){
+            month++;
+            if(month == months.length){
+                month = 0;
+                year++;
+            }
+            $("td").empty();
+            displayMonth();
+        }
+        else if(($(this).hasClass("previous"))){
+            month--;
+            if(month == -1){
+                month = months.length -1;
+                year--;
+            }
+            $("td").empty();
+            displayMonth();
+        }
+    });
+    
+    $("#save").click(function(){
+        addEvent();
+        showEvents();
+        $("#myModal").modal("hide");
+    });
 
-$(".icon").click(function(){
-    if($(this).hasClass("next")){
-        month++;
-        if(month == months.length){
-            month = 0;
-            year++;
-        }
-        $("td").empty();
-        displayMonth();
+    $("#addToDo").popover({
+        placement: "bottom",
+        title: "Add To Do Item",
+        html: true,
+        content: $("#formToDo").html()
+    }).click(function(){
+        $("#saveToDo").click(function(){
+            let li = document.createElement("li");
+            li.classList.add("list-group-item");
+            li.innerHTML = $("#toDo").val();
+            $(".listToDo").append(li);
+            $("#addToDo").popover("hide");
+        })  
+    })
+
+    $("#addReminder").popover({
+        placement: "bottom",
+        title: "Add a reminder",
+        html: true,
+        content: $("#formReminder").html()
+    }).click(function(){
+        $("#saveReminder").click(function(){
+            let li = document.createElement("li");
+            li.classList.add("list-group-item");
+            li.innerHTML = $("#reminder").val();
+            $(".listReminder").append(li);
+            $("#addReminder").popover("hide");
+        })  
+    })
+
+    $('#datetimepicker1').datetimepicker();
+    $("#datetimepicker1").data("DateTimePicker").sideBySide(true).showClose(true);
+    $('#datetimepicker1').data("DateTimePicker").format("DD/MM/YYYY, kk")
+    console.log( $('#datetimepicker1').data("DateTimePicker").format());
+
+    $(document).on("show.bs.modal", function(){
+        clearModal();
+        addEvent();
+    });
+    getData();
+    displayMonth();
+
+})
+
+
+
+
+function addEvent(){
+    
+    let text = $("#txtEvent").val();
+    let selectedDate = formatDate(new Date($("#date").val()));
+    let li = document.createElement("li");
+    li.innerHTML = text;
+    li.classList.add("list-group-item");
+    li.classList.add("list-group-item-info");
+    let x = new calenderItem(text,selectedDate,li);
+    saveData(x);
+} 
+function clearModal(){
+    $("#txtEvent").val("");
+}
+
+function formatDate(date){
+    let day = date.getDate();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    return new dateTime(day,month,year,hours,minutes);
+}
+
+function getData(){
+    if(JSON.parse(localStorage.getItem("events")==null)){
+        this.eventList = [];
     }
-    else if(($(this).hasClass("previous"))){
-        month--;
-        if(month == -1){
-            month = months.length -1;
-            year--;
-        }
-        $("td").empty();
-        displayMonth();
-    }
-});
+   else{
+    this.eventList = JSON.parse(localStorage.getItem("events"));
+   } 
+}
+
+function saveData(newEvent){
+    getData();
+    eventList.push(newEvent);
+    localStorage.setItem("events", JSON.stringify(eventList));
+}
+
 
 function daysOfMonth(month,year){
     if((month == 1)&&(year % 4 == 0)&&(year % 100 == 0)&&(year % 400 == 0)){
@@ -34,6 +145,21 @@ function daysOfMonth(month,year){
     else{
         return days[month];
     }
+}
+
+function showEvents(){
+    let i = 1;
+    $("td").each(function(){
+        $(this).find("ul").empty();
+        for(item of eventList){
+            if((item.date.month==month)&&(item.date.year == year)){
+                if(item.date.day == i){
+                    $(this).find("ul").append(item.element);
+                }
+            }
+        }
+        i++;
+    })
 }
 
 function displayMonth(){
@@ -53,66 +179,44 @@ function displayMonth(){
         }
 
         let td = $("._"+ week).children()[startDay];
+        //td.append(document.createElement("p").innerHTML = j+1);
         td.innerHTML = j+1;
-        /*let td = document.createElement("td");
-        td.innerHTML = j+1;
-        $("._"+ week).append(td);*/
         startDay++;
         if(startDay ==  7){
             startDay = 0 ;
         }
-    }
-    addList();
+    } 
+    fillTable();
+    console.log(eventList);
+    showEvents();
 }
 
-function addList(){
-    let list = document.createElement("ul");
-    $("table ul").addClass("list-group");
-
-    let tds = $("td");
-    for(td of tds){
-        if(td.innerHTML.length != 0){
-            td.append(list); 
-            td.click(function(){
-                let listItem = document.createElement("li");
-                listItem.innerHTML="Test";
-                //$("table li").addClass("list-group-item");   
-                $(this).find("ul").append(listItem);
-                }); 
-            } 
-        }
-    }
-    /*$("td").each(function(){
-        console.log($(this).html().length);
-        if($(this).html().length != 0){
-            $(this).append(list); 
+function fillTable(){
+    $("td").each(function(){
+        if($(this).html().length > 0){
+            let ul = document.createElement("ul");
+            ul.classList.add("list-group-flush")
+            $(this).append(ul);
             $(this).click(function(){
-                let listItem = document.createElement("li");
-                listItem.innerHTML="Test";
-                //$("table li").addClass("list-group-item");   
-                $(this).find("ul").append(listItem);
-                }); 
-            } 
-        });*/
+                let clicked = $(this).text();
+                console.log(clicked);
+                $("#datetimepicker1").data("DateTimePicker").date(new Date(clicked,month,year));
+                $("#myModal").modal({
+                    backdrop: "static",
+                    keyboard:true
+                });
+            })
+        }
+    });
+}
+/*  $(function) () {
+                $('#datetimepicker1').datetimepicker({
+                    dateFormat: "dd-mm-yyyy"}).datetimepicker("setDate", new Date(clicked,month,year));
+                });
+                });*/
 
-//if
-//console.log($("td").children().length);
 
 
-/*if($("td").children().length != 0){
-    $("td").append(list); 
-    $("td").click(function(){
-        let listItem = document.createElement("li");
-        listItem.innerHTML="Test";
-        //$("table li").addClass("list-group-item");   
-        $(this).find("ul").append(listItem);
-        }) 
-    }  
-}*/
-
-/*if(($("td").html() !== null)||($("td").html() !== undefined)){
-     
-}*/
     
 
 
---
+++ events.js
@@ -0,0 +1,22 @@
+/*[
+    {
+        "text": "Meeting with customer",
+        "date": {
+            "day" : "day",
+            "hours" : "hours",
+            "minutes": "minutes",
+            "month": "month",
+            "year" : "year"
+        }
+    },
+    {
+        "text": "Meeting at this place",
+        "date": {
+            "day" : "day",
+            "hours" : "hours",
+            "minutes": "minutes",
+            "month": "month",
+            "year" : "year"
+        }
+    }
+]*/
