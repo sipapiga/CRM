02c5e986a49076a685a4ddfcbdac5e55785537a5
thelopster <nitipon.apaisri@gmail.com>
2019-11-01 13:58:26 +0100

    Changed birthday to todolist

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,14 +1,14 @@
 //Det här behöver sortering datum***
-function showCustomerBirthday(cus) {
-    const cusBD = document.querySelector("#birthdayList");
-    const row = document.createElement("tr");
-    row.innerHTML = `
-              <td><p>${cus.DOB}</p></td>
-              <td><p >${cus.name}</p></td>
-              <td><p >${cus.company} </p></td>
-           `;
-    cusBD.appendChild(row);
-  }
+// function showCustomerBirthday(cus) {
+//     const cusBD = document.querySelector("#birthdayList");
+//     const row = document.createElement("tr");
+//     row.innerHTML = `
+//               <td><p>${cus.DOB}</p></td>
+//               <td><p >${cus.name}</p></td>
+//               <td><p >${cus.company} </p></td>
+//            `;
+//     cusBD.appendChild(row);
+//   }
   function showEvent() {
     let getEvents = JSON.parse(localStorage.getItem("events"));
     const eventsNum = document.querySelector("#events");
@@ -40,7 +40,7 @@ function showCustomerBirthday(cus) {
     } else {
       antalCustomer.innerHTML = contactList.length;
       for (let x = 0; x < contactList.length; x++) {
-        showCustomerBirthday(contactList[x]);
+        // showCustomerBirthday(contactList[x]);
       }
     }
   }
@@ -100,6 +100,7 @@ function showCustomerBirthday(cus) {
   
   function showToDo() {
     const toDoTable = document.querySelector("#toDoText");
+    const todoNo = document.querySelector('#todoNo');
     toDoTable.innerHTML = "";
     for (item of list) {
       const row = document.createElement("tr");
@@ -108,6 +109,7 @@ function showCustomerBirthday(cus) {
           <td><button class="btn btn-danger btn-sm deleteToDo">Delete</button></tr>
           `;
       toDoTable.appendChild(row);
+      todoNo.innerHTML = list.length;
     }
     $(".deleteToDo").each(function() {
       $(this).on("click", function() {
