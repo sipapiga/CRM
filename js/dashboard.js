d0c7d7ba6ab7e04d90339c8109c70c7b940aca3a
BarbaraMaes <maesbarbara92@gmail.com>
2019-11-03 16:45:49 +0100

    Changed display of times

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,3 @@
-
 //Det här behöver sortering datum***
 // function showCustomerBirthday(cus) {
 //     const cusBD = document.querySelector("#birthdayList");
@@ -10,135 +9,142 @@
 //            `;
 //     cusBD.appendChild(row);
 //   }
-  function showEvent() {
-    let getEvents = JSON.parse(localStorage.getItem("events"));
-    const eventsNum = document.querySelector("#events");
-    eventsNum.innerHTML = getEvents.length;
-    let sortEvent = getEvents.sort();
-    console.log(sortEvent);
-  
-    for (let event of getEvents) {
-      appendEvent(event);
+function showEvent() {
+  let getEvents = JSON.parse(localStorage.getItem("events"));
+  const eventsNum = document.querySelector("#events");
+  eventsNum.innerHTML = getEvents.length;
+  let sortEvent = getEvents.sort();
+  console.log(sortEvent);
+
+  for (let event of getEvents) {
+    appendEvent(event);
+  }
+}
+function appendEvent(event) {
+  const eventsDiv = document.querySelector("#eventText");
+  const row = document.createElement("tr");
+  let hours = event.date.hours;
+  let minutes = event.date.minutes;
+
+  if (event.date.hours < 10) {
+    hours = "0" + event.date.hours;
+  }
+  if (event.date.minutes < 10) {
+    minutes = "0" + event.date.minutes;
+  }
+  row.innerHTML = `
+          <td>${event.text}</td>
+          <td>${event.date.month + 1}${"/"}${event.date.day}${"/"}${
+    event.date.year
+  }</td>
+          <td>${hours}${":"}${minutes}</td></tr>
+          `;
+  eventsDiv.appendChild(row);
+}
+function showCustomer() {
+  let contactList = JSON.parse(localStorage.getItem("Customers"));
+  const antalCustomer = document.querySelector("#customers");
+  if (contactList === null) {
+    antalCustomer.innerHTML = 0;
+  } else {
+    antalCustomer.innerHTML = contactList.length;
+    for (let x = 0; x < contactList.length; x++) {
+      // showCustomerBirthday(contactList[x]);
     }
   }
-  function appendEvent(event) {
-    const eventsDiv = document.querySelector("#eventText");
+}
+class toDo {
+  constructor(id, text) {
+    this.id = id;
+    this.text = text;
+  }
+}
+class toDoList {
+  constructor() {
+    this.id = 1;
+    this.list = [];
+  }
+}
+
+let newList = new toDoList();
+
+function getData() {
+  if (
+    JSON.parse(localStorage.getItem("todo")) != null &&
+    JSON.parse(localStorage.getItem("todoID") != null)
+  ) {
+    this.list = JSON.parse(localStorage.getItem("todo"));
+    newList.id = JSON.parse(localStorage.getItem("todoID"));
+  } else {
+    this.list = [];
+  }
+  return this.list;
+}
+function saveData(newEvent) {
+  console.log(newEvent);
+  this.list = getData();
+  if (newEvent != undefined && newEvent.text != "") {
+    list.push(newEvent);
+    newList.id++;
+  }
+  localStorage.setItem("todoID", JSON.stringify(newList.id));
+  localStorage.setItem("todo", JSON.stringify(this.list));
+  this.list = getData();
+  showToDo();
+}
+
+function removeItem(id) {
+  for (item of list) {
+    if (id == item.id) {
+      let place = list.indexOf(item);
+      list.splice(place, 1);
+      newList.id--;
+    }
+  }
+  localStorage.setItem("todoID", JSON.stringify(newList.id));
+  localStorage.setItem("todo", JSON.stringify(this.list));
+  saveData();
+}
+
+function showToDo() {
+  const toDoTable = document.querySelector("#toDoText");
+  const todoNo = document.querySelector("#todoNo");
+  toDoTable.innerHTML = "";
+  for (item of list) {
     const row = document.createElement("tr");
     row.innerHTML = `
-          <td>${event.text}</td>
-          <td>${event.date.month}${"/"}${event.date.day}${"/"}${
-      event.date.year
-    }</td>
-          <td>${event.date.hours}${":"}${event.date.minutes}</td></tr>
-          `;
-    eventsDiv.appendChild(row);
-  }
-  function showCustomer() {
-    let contactList = JSON.parse(localStorage.getItem("Customers"));
-    const antalCustomer = document.querySelector("#customers");
-    if (contactList === null) {
-      antalCustomer.innerHTML = 0;
-    } else {
-      antalCustomer.innerHTML = contactList.length;
-      for (let x = 0; x < contactList.length; x++) {
-        // showCustomerBirthday(contactList[x]);
-      }
-    }
-  }
-  class toDo {
-    constructor(id, text) {
-      this.id = id;
-      this.text = text;
-    }
-  }
-  class toDoList {
-    constructor() {
-      this.id = 1;
-      this.list = [];
-    }
-  }
-  
-  let newList = new toDoList();
-  
-  function getData() {
-    if (
-      JSON.parse(localStorage.getItem("todo")) != null &&
-      JSON.parse(localStorage.getItem("todoID") != null)
-    ) {
-      this.list = JSON.parse(localStorage.getItem("todo"));
-      newList.id = JSON.parse(localStorage.getItem("todoID"));
-    } else {
-      this.list = [];
-    }
-    return this.list;
-  }
-  function saveData(newEvent) {
-    console.log(newEvent);
-    this.list = getData();
-    if (newEvent != undefined && newEvent.text != "") {
-      console.log("pushing item");
-      list.push(newEvent);
-      newList.id++;
-    }
-    localStorage.setItem("todoID", JSON.stringify(newList.id));
-    localStorage.setItem("todo", JSON.stringify(this.list));
-    this.list = getData();
-    showToDo();
-  }
-  
-  function removeItem(id) {
-    for (item of list) {
-      if (id == item.id) {
-        let place = list.indexOf(item);
-        list.splice(place, 1);
-        newList.id--;
-      }
-    }
-    localStorage.setItem("todoID", JSON.stringify(newList.id));
-    localStorage.setItem("todo", JSON.stringify(this.list));
-    saveData();
-  }
-  
-  function showToDo() {
-    const toDoTable = document.querySelector("#toDoText");
-    const todoNo = document.querySelector('#todoNo');
-    toDoTable.innerHTML = "";
-    for (item of list) {
-      const row = document.createElement("tr");
-      row.innerHTML = `
           <td id="${item.id}"><p>${item.text}</p></td>
           <td><button class="btn btn-danger btn-sm deleteToDo">Delete</button></tr>
           `;
-      toDoTable.appendChild(row);
-      todoNo.innerHTML = list.length;
-    }
-    $(".deleteToDo").each(function() {
-      $(this).on("click", function() {
-        $(this)
-          .parent()
-          .siblings()
-          .each(function() {
-            removeItem($(this).attr("id"));
-          });
-      });
-    });
+    toDoTable.appendChild(row);
+    todoNo.innerHTML = list.length;
   }
-  
-  $(document).ready(function() {
-    saveData();
-    $("#modalToDo").on("show.bs.modal", function(event) {
-      let modal = $(this);
-      modal.find("#save").click(function() {
-        let x = new toDo(newList.id, $("#toDo").val());
-        $("#toDo").val("");
-        saveData(x);
-        modal.modal("toggle");
-      });
+  $(".deleteToDo").each(function() {
+    $(this).on("click", function() {
+      $(this)
+        .parent()
+        .siblings()
+        .each(function() {
+          removeItem($(this).attr("id"));
+        });
     });
   });
-  
-  window.addEventListener("DOMContentLoaded", event => {
-    showCustomer();
-    showEvent();
+}
+
+$(document).ready(function() {
+  saveData();
+  $("#modalToDo").on("show.bs.modal", function(event) {
+    let modal = $(this);
+    modal.find("#save").click(function() {
+      let x = new toDo(newList.id, $("#toDo").val());
+      $("#toDo").val("");
+      saveData(x);
+      modal.modal("toggle");
+    });
   });
-  
+});
+
+window.addEventListener("DOMContentLoaded", event => {
+  showCustomer();
+  showEvent();
+});
